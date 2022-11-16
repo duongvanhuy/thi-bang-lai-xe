4802a43596655db08ec5a8c462f1587e54a6e4f4
duongvanhuy <duongvanhuy137@gmail.com>
2022-11-16 16:36:28 +0700

    feat: Add Role

---
+++ GUB.TracNghiThiBangLai.EntityFrameworkCore/Migrations/20221116090418_Add Role.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GUB.TracNghiThiBangLai.EntityFrameworkCore.Migrations
+{
+    public partial class AddRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
---
+++ GUB.TracNghiemThiBangLai.Entities/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUB.TracNghiemThiBangLai.Entities
+{
+    public class Role : BaseEnitities
+    {
+        public string Name { get; set; }
+        public int Type { get; set; }
+       
+    }
+}
